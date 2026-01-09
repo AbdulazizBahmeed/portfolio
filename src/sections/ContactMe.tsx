69ef37c504f45548a790ab45493501b83e37e1ee
abdulaziz bahmeed <abdulazizbahmeed@outlook.sa>
2026-01-10 02:14:26 +0300

    added the validation step and message

--- src/sections/ContactMe.tsx
+++ src/sections/ContactMe.tsx
@@ -1,7 +1,79 @@
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function ContactMe() {
   const { t } = useTranslation();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  function validate() {
+    let isSucceeded = true;
+    const newErrors = {
+      name: "",
+      email: "",
+      message: "",
+    };
+
+    if (!form.name.trim()) {
+      newErrors.name = "Name is required";
+      isSucceeded = false;
+    }
+
+    if (!form.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(form.email)) {
+      newErrors.email = "Invalid email address";
+      isSucceeded = false;
+    }
+
+    if (!form.message.trim()) {
+      newErrors.message = "Message is required";
+      isSucceeded = false;
+    }
+    console.log(newErrors);
+
+    setErrors(newErrors);
+    return isSucceeded;
+  }
+
+  async function handleSend() {
+    if (!validate()) return;
+    setErrors({
+      name: "",
+      email: "",
+      message: "",
+    });
+    setLoading(true);
+
+    try {
+      //   const response = await fetch("https://api.example.com/contact", {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify(form),
+      //   });
+
+      //   if (!response.ok) {
+      //     alert("Something went wrong with server. Please try again.");
+      //   }
+
+      setForm({ name: "", email: "", message: "" });
+    } catch (error) {
+      alert("Something went wrong. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
@@ -21,21 +93,41 @@ function ContactMe() {
             <div className="bg-gray-900 rounded-lg p-8 shadow-2xl border border-gray-800">
               <div className="space-y-4">
                 <input
-                  type="text"
+                  className="w-full px-4 py-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
                   placeholder={t("contact.name")}
-                  className="w-full px-4 py-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
+                  type="text"
+                  value={form.name}
+                  onChange={(e) => setForm({ ...form, name: e.target.value })}
                 />
+                {errors.name && (
+                  <p className="px-3 text-red-500 text-sm">{errors.name}</p>
+                )}
                 <input
-                  type="email"
+                  className="w-full px-4 py-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
                   placeholder={t("contact.email")}
-                  className="w-full px-4 py-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
+                  type="email"
+                  value={form.email}
+                  onChange={(e) => setForm({ ...form, email: e.target.value })}
                 />
+                {errors.email && (
+                  <p className="px-3 text-red-500 text-sm">{errors.email}</p>
+                )}
                 <textarea
+                  className="w-full px-4 pt-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
                   placeholder={t("contact.message")}
-                  rows={6}
-                  className="w-full px-4 py-3 bg-black border border-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 text-white"
+                  value={form.message}
+                  onChange={(e) =>
+                    setForm({ ...form, message: e.target.value })
+                  }
+                  rows={5}
                 ></textarea>
-                <button className="w-full bg-gradient-to-r from-red-600 to-red-800 text-white py-3 rounded-lg hover:shadow-lg hover:shadow-red-500/50 transition">
+                {errors.message && (
+                  <p className="px-3 pb-3 text-red-500 text-sm">{errors.message}</p>
+                )}
+                <button
+                  className="w-full bg-gradient-to-r from-red-600 to-red-800 text-white py-3 rounded-lg hover:shadow-lg hover:shadow-red-500/50 transition"
+                  onClick={() => handleSend()}
+                >
                   {t("contact.send")}
                 </button>
               </div>
